ff0f7205b3373a67356cc0409de047009cc19b31
Pulho <pvoa@cin.ufpe.br>
2019-09-23 16:05:03 -0300

    Remocao de arquivos nao mais utilizados

--- setup.py
+++
@@ -1,7 +0,0 @@
-try:
-	from config.config import __prefix__, __token__
-	print('Import prefix and token from config, sucess')
-except ImportError:
-	print('Import prefix and token error')
-	__prefix__ = '!'
-	__token__ = 'NDA4NjU2ODk2NDk3NTQ5MzEy.DVTO7w.3SKVdlK6_1OLI8Jx0u7-UHIGpFY'

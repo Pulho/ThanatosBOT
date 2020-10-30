8755443efaf2288f8413c7063ebccffbfd3bf159
Pulho <pulholiveira@gmail.com>
2020-10-30 16:14:45 -0300

    Adição secret Token Thanatos

--- config/setup.py
+++ config/setup.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 
 __prefix__ = '!'
-__token__ = 'NDA4NjU2ODk2NDk3NTQ5MzEy.DVTO7w.3SKVdlK6_1OLI8Jx0u7-UHIGpFY'
+__token__ = ${{ secrets.THANATOS_TOKEN }}
 
 bot = commands.Bot(command_prefix = __prefix__)
 bot.remove_command("help")

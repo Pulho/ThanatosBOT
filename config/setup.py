8b0cbf7441ae98b7d085bfac2d0892de5b6277b5
Pulho <pulholiveira@gmail.com>
2020-11-02 21:45:36 -0300

    Adição do token do watch2gether

--- config/setup.py
+++ config/setup.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 
 __prefix__ = '!'
 __token__ = ${{ secrets.THANATOS_TOKEN }}
+ __w2g__token__ = ${{ secrets.W2G_TOKEN }}
 
 bot = commands.Bot(command_prefix = __prefix__)
 bot.remove_command("help")
